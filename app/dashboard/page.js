84c0368b9714da27841d22747fca48f254ee0408
SOURAV JENA <souravjena466@gmail.com>
2026-04-28 00:45:46 +0530

    Update page.js

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -126,7 +126,7 @@ const Dashboard = () => {
         {/* input for email */}
         <div className="my-2">
           <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-          <input value={form?.email || ""} readOnly type="email" name='email' id="email" className="block w-full cursor-not-allowed p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-100 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
+          <input value={form?.email || ""} type="email" name='email' id="email" className="block w-full cursor-not-allowed p-2 text-gray-900 border border-gray-300 rounded-lg bg-gray-100 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" />
         </div>
         {/* input forusername */}
         <div className='my-2'>
