04ec0846f8326c9a3d7e45677a3577e007116b08
SOURAV JENA <souravjena466@gmail.com>
2026-04-28 01:07:03 +0530

    Update connectDB.js

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,23 +1,46 @@
 
+// import mongoose from "mongoose";
+
+// const connectDb = async () => {
+//     if (!process.env.MONGO_URI) {
+//         throw new Error("Missing MONGO_URI in environment variables");
+//     }
+
+//     if (mongoose.connection.readyState === 1) {
+//         return mongoose.connection;
+//     }
+
+//     try {
+//         const conn = await mongoose.connect(process.env.MONGO_URI);
+//         console.log(`MongoDB Connected: ${conn.connection.host}`);
+//         return conn;
+//     } catch (error) {
+//         console.error("MongoDB connection error:", error.message);
+//         throw error;
+//     }
+// }
+
+//   export default connectDb;
+
+
 import mongoose from "mongoose";
 
+let isConnected = false;
+
 const connectDb = async () => {
-    if (!process.env.MONGO_URI) {
-        throw new Error("Missing MONGO_URI in environment variables");
-    }
+  if (isConnected) return;
 
-    if (mongoose.connection.readyState === 1) {
-        return mongoose.connection;
-    }
+  try {
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+      bufferCommands: false,
+    });
 
-    try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
-        return conn;
-    } catch (error) {
-        console.error("MongoDB connection error:", error.message);
-        throw error;
-    }
-}
+    isConnected = db.connections[0].readyState;
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    throw error;
+  }
+};
 
-  export default connectDb;
+export default connectDb;
